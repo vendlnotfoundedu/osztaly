f19f25a3a296439c06c4f7237d49d0ab58fa4e38
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-17 11:40:37 +0200

    atlag

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using osztaly;
 
 Auto kek = new Auto(2020, "Focus", "Ford");
-Auto zold = new Auto(3100, "Golf", "Volkswagen");
+Auto zold = new Auto(1998, "Golf", "Volkswagen");
 Auto fekete = new Auto(2010, "Tiguan", "Volkswagen");
 
 List <Auto> garazs = new List<Auto> { kek, zold, fekete};
@@ -9,3 +9,8 @@ foreach (Auto seged in garazs)
 {
     seged.KiirAdatok();
 }
+
+//átlag
+double atlag = garazs.Average(x => x.GyartasiEv);
+Console.WriteLine($"Átlag gyártási év: {atlag}");
+//Console.WriteLine($"Legfiatalabb autó: {garazs.Max(g => g.GyartasiEv)}");

9fa48166428f23b8fb948291f9c331bc8674f9f3
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-17 11:28:14 +0200

    3. fa

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using osztaly;
 
-Auto kek = new Auto(2020, "Ford", "Focus");
-Auto zold = new Auto(3100, "Voolkswagen", "Golf");
+Auto kek = new Auto(2020, "Focus", "Ford");
+Auto zold = new Auto(3100, "Golf", "Volkswagen");
+Auto fekete = new Auto(2010, "Tiguan", "Volkswagen");
 
-//Console.WriteLine($"Kék autó: {kek.Marka} {kek.Modell}, gyártási év: {kek.GyartasiEv}");
-//Console.WriteLine($"Zöld autó: {zold.Marka} {zold.Modell}, gyártási év: {zold.GyartasiEv}");
+List <Auto> garazs = new List<Auto> { kek, zold, fekete};
+foreach (Auto seged in garazs)
+{
+    seged.KiirAdatok();
+}

8f79ce843bf59dda547d0999465d9810f9a1b406
vendlnotfoundedu <nagy.vendel.2023d@telekimezotur.hu>
2026-09-17 11:15:27 +0200

    konstruktor

---
+++ Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace osztaly
+{
+    internal class Auto
+    {
+        private int gyartasiEv;
+        public string Marka { get; set; }
+        public string Modell { get; set; }
+        public int GyartasiEv
+        {
+            get { return gyartasiEv; }
+            set { if (value > 1886 && value <= 2026)
+                {
+                    gyartasiEv = value;
+                }
+                else gyartasiEv = 0;
+            }
+        }
+        public Auto(int GyartasiEv, string modell, string marka)
+        {
+            this.GyartasiEv = GyartasiEv;
+            Modell = modell;
+            Marka = marka;
+        }
+        public void KiirAdatok()
+        {
+            Console.WriteLine($"{Marka} {Modell} - Gyártási év: {GyartasiEv}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using osztaly;
+
+Auto kek = new Auto(2020, "Ford", "Focus");
+Auto zold = new Auto(3100, "Voolkswagen", "Golf");
+
+//Console.WriteLine($"Kék autó: {kek.Marka} {kek.Modell}, gyártási év: {kek.GyartasiEv}");
+//Console.WriteLine($"Zöld autó: {zold.Marka} {zold.Modell}, gyártási év: {zold.GyartasiEv}");
